5395e536e11546e9d6912be6ae01e56bbe1ce382
BRIGHTCOMPUTERS <brightcomputersinfo@gmail.com>
2020-03-20 11:01:57 +0530

    mobile number modified

--- student/models.py
+++ student/models.py
@@ -13,7 +13,7 @@ class StudentDetails(models.Model):
     email = models.CharField(max_length=50,null=True)
     qualification = models.CharField(max_length=20,null=True)
     address = models.CharField(max_length=20,null=True)
-    phoneno = models.PositiveIntegerField(null=True)
+    phoneno = models.CharField(max_length=12,null=True)
 
     def __str__(self):
         return self.name
