bed962df186b03bc71aaec58e0589f19c2ee36ee
brightcomputers <61632624+brightcomputers@users.noreply.github.com>
2020-04-03 10:59:42 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title=models.CharField(max_length=30)
-    content=models.TextField()
+    content=models.CharField(max_length=1000)
     date_posted=models.DateTimeField(default=timezone.now)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self):
